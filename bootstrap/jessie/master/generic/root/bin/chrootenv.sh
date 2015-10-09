e7436caf18513e648146f63d8ae72f20363c088c
fmgborg <fg-github.2014@datenschutzraum.org>
2015-10-09 14:57:24 +0200

    command line output: on or off

--- bootstrap/jessie/master/generic/root/bin/chrootenv.sh
+++ bootstrap/jessie/master/generic/root/bin/chrootenv.sh
@@ -5,6 +5,7 @@ TODO=$1
 MOUNTPOINT="/mnt"
 
 off() {
+echo "chrootenv off"
 umount ${MOUNTPOINT}/sys
 umount ${MOUNTPOINT}/proc
 umount ${MOUNTPOINT}/dev/pts
@@ -12,10 +13,30 @@ umount ${MOUNTPOINT}/dev
 }
 
 on() {
-mount -o bind /dev "${MOUNTPOINT}/dev"
-mount -o bind /dev/pts "${MOUNTPOINT}/dev/pts"
-mount -o bind /proc "${MOUNTPOINT}/proc"
-mount -o bind /sys "${MOUNTPOINT}/sys"
+echo "chrootenv on"
+mount -o bind /dev	"${MOUNTPOINT}/dev"
+mount -o bind /dev/pts	"${MOUNTPOINT}/dev/pts"
+mount -o bind /proc	"${MOUNTPOINT}/proc"
+mount -o bind /sys	"${MOUNTPOINT}/sys"
+}
+
+alternate() {
+# just documentation
+mount -t devfs devfs	/mnt//dev
+mount -o bind /dev/pts	/mnt//dev/pts
+mount -t proc  proc	/mnt/proc/
+mount -t sysfs sys	/mnt/sys/
+}
+
+inside_chroot() {
+# just documentation
+mount -t devfs none /dev
+mount -t proc  none /proc
+}
+
+outside_chroot() {
+# just documentation
+mount -t proc none /mnt/proc
 }
 
 case ${TODO} in
