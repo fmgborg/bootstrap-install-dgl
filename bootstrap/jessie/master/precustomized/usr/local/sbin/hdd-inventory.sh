38cfa5dd33febfb93657b05c2206c5e643bf1cbb
fmgborg <fg-github.2014@datenschutzraum.org>
2016-09-24 23:01:19 +0200

    save original data from new hard drive with rsync

--- bootstrap/jessie/master/precustomized/usr/local/sbin/hdd-inventory.sh
+++ bootstrap/jessie/master/precustomized/usr/local/sbin/hdd-inventory.sh
@@ -2,7 +2,7 @@
 
 # author                                : fmg
 # doc   (date of creation)              : 2016-09-24 21:56
-# dolm  (date of last modification)     : 2016-09-24 22:42
+# dolm  (date of last modification)     : 2016-09-24 22:58
 
 SDX="sdc"
 DEVICE="/dev/${SDX}"
@@ -16,30 +16,33 @@ cd ${SUBDIR}
 # MBR
 dd if=${DEVICE} of=mbr.bin bs=512 count=1
 
-fdisk -l ${DEVICE}                              > fdisk-l.txt
-fdisk.distrib -l ${DEVICE}                      > fdisk.distrib-l.txt
-gdisk -l ${DEVICE}                              > gdisk-l.txt
-parted ${DEVICE} print                          > parted-print.txt
+fdisk -l ${DEVICE}				> fdisk-l.txt
+fdisk.distrib -l ${DEVICE}			> fdisk.distrib-l.txt
+gdisk -l ${DEVICE}				> gdisk-l.txt
+parted ${DEVICE} print				> parted-print.txt
 
 # disk free
 mount -o ro ${DEVICE}1 /mnt
 #
-LANG=de_DE.UTF-8 df -h | grep -E "(^Da|${SDX})" > df-h.de.txt
-LANG=de_DE.UTF-8 df -m | grep -E "(^Da|${SDX})" > df-m.de.txt
-LANG=de_DE.UTF-8 df | grep -E "(^Da|${SDX})"    > df.de.txt
+LANG=de_DE.UTF-8 df -h | grep -E "(^Da|${SDX})"	> df-h.de.txt
+LANG=de_DE.UTF-8 df -m | grep -E "(^Da|${SDX})"	> df-m.de.txt
+LANG=de_DE.UTF-8 df | grep -E "(^Da|${SDX})"	> df.de.txt
 #
-LANG=C df -h | grep -E "(^F|${SDX})"            > df-h.en.txt
-LANG=C df -m | grep -E "(^F|${SDX})"            > df-m.en.txt
-LANG=C df | grep -E "(^F|${SDX})"               > df.en.txt
+LANG=C df -h | grep -E "(^F|${SDX})"		> df-h.en.txt
+LANG=C df -m | grep -E "(^F|${SDX})"		> df-m.en.txt
+LANG=C df | grep -E "(^F|${SDX})"		> df.en.txt
 #
 ###umount /mnt
 # mount
 ###mount -o ro ${DEVICE}1 /mnt
-mount | grep ${SDX}                             > mount-ro.txt
+mount | grep ${SDX}				> mount-ro.txt
+D_SUBDIR="data-ro-${NOW}"
+mkdir -p "../${D_SUBDIR}"
+rsync -a /mnt/ "../${D_SUBDIR}"
 umount /mnt
 #
 mount ${DEVICE}1 /mnt
-mount | grep ${SDX}                             > mount.txt
+mount | grep ${SDX}				> mount.txt
 umount /mnt
 
 # EOF
