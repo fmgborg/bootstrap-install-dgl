fe0cf6f35a191b5079842e7d3f509b6e3140a21c
fmgborg <fg-github.2014@datenschutzraum.org>
2018-07-11 14:08:47 +0200

    add legacy "ifconfig" to stretch

--- bootstrap/stretch/master/generic/sbin/run-once-stage.00.01-pre-user.sh
+++ bootstrap/stretch/master/generic/sbin/run-once-stage.00.01-pre-user.sh
@@ -874,7 +874,8 @@ ${INSTALL} uuid-runtime
 #${INSTALL} x11.utils # libfontenc1{a} libxaw7{a} libxcb-shape0{a} libxmu6{a} libxpm4{a} libxt6{a} libxv1{a} libxxf86dga1{a}
 #${INSTALL} x11-utils # libfontenc1{a} libxaw7{a} libxcb-shape0{a} libxmu6{a} libxpm4{a} libxt6{a} libxv1{a} libxxf86dga1{a}
 
-#${INSTALL} .
+${INSTALL} net-tools	# old legacy ifconfig
+
 #${INSTALL} .
 #${INSTALL} .
 #${INSTALL} .
